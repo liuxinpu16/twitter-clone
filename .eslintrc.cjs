6160cf42b1261bfb2d00e89c58063cec22182ac0
Xinpu <liuxinpu16@gmail.com>
2023-05-31 21:34:05 -0700

    v1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,14 @@ const config = {
       parserOptions: {
         project: path.join(__dirname, "tsconfig.json"),
       },
+      rules: {
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-argument": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/restrict-plus-operands": "off",
+      },
     },
   ],
   parser: "@typescript-eslint/parser",
---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,95 @@
+import { useSession } from "next-auth/react";
+import Button from "./Button";
+import ProfileImage from "./ProfileImage";
+import {
+  useLayoutEffect,
+  useState,
+  useRef,
+  useCallback,
+  type FormEvent,
+} from "react";
+import { api } from "~/utils/api";
+
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+  if (textArea == null) return;
+  textArea.style.height = "0";
+  textArea.style.height = `${textArea.scrollHeight}px`;
+}
+
+export function Form() {
+  const session = useSession();
+  const [inputValue, setInputValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+  const trpcUtils = api.useContext();
+
+  useLayoutEffect(() => updateTextAreaSize(textAreaRef.current), [inputValue]);
+  if (session.status !== "authenticated") return null;
+
+  const createTweet = api.tweet.create.useMutation({
+    onSuccess: (newTweet) => {
+      setInputValue("");
+      if (session.status !== "authenticated") return;
+
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+        if (oldData == null || oldData.pages[0] == null) return;
+        const newCatcheTweet = {
+          ...newTweet,
+          likeCount: 0,
+          likedByMe: false,
+          user: {
+            id: session.data.user.id,
+            name: session.data.user.name || null,
+            image: session.data.user.image || null,
+          },
+        };
+        return {
+          ...oldData,
+          pages: [
+            {
+              ...oldData.pages[0],
+              tweets: [newCatcheTweet, ...oldData.pages[0].tweets],
+            },
+            ...oldData.pages.slice(1),
+          ],
+        };
+      });
+    },
+  });
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    createTweet.mutate({ content: inputValue });
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 border-b px-4 py-2"
+    >
+      <div className="gap flex">
+        <ProfileImage src={session.data.user.image} />
+        <textarea
+          ref={inputRef}
+          style={{ height: 0 }}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="What's happening"
+          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+        />
+      </div>
+      <Button className="self-end">Tweet</Button>
+    </form>
+  );
+}
+
+const NewTweetForm = () => {
+  const session = useSession();
+  if (session.status !== "authenticated") return null;
+  return <Form />;
+};
+
+export default NewTweetForm;
